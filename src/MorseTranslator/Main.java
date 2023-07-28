d4d57e25890aea3dde5b4f232053dbebfe13db59
Mike <mike.b26432@outlook.com>
2023-07-28 13:49:10 +0100

    first commit

---
+++ src/MorseTranslator/Main.java
@@ -0,0 +1,8 @@
+package Main;
+
+public class Main {
+    public static void main(String[] args) {
+        MorseTranslator translator = new MorseTranslator();
+        System.out.println(translator.translateEnglish(".--- .- ...- .- / .. ... / -.-. --- --- .-.. / --- -.-"));
+    }
+}
---
+++ src/MorseTranslator/MorseTranslator.java
@@ -0,0 +1,2 @@
+package MorseTranslator;public class MorseTranslator {
+}
